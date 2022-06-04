ec6246b7a29339a0e8bddb4e7ff7fb3696e6b97c
Hemalkumar1 <106871261+Hemalkumar1@users.noreply.github.com>
2022-06-04 08:37:51 -0400

    Add files via upload

---
+++ Fundamentals of Java/src/Hemal.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author hemal
+ *
+ */
+public class Hemal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Hemal");
+	}
+
+}
